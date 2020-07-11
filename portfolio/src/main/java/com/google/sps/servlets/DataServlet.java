50f61ea603607b4b9b43ac6dffe0340e83f2422a
Catherine <chen.yc.catherine@gmail.com>
2020-07-11 20:01:21 +0800

    Implement doGet and doPost

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -27,9 +27,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   
-  private List<String> greetings;
+  //private List<String> greetings;
 
-  @Override
+  /*@Override
   public void init() {
     greetings = new ArrayList<>();
     greetings.add("Nice to meet you");
@@ -41,14 +41,71 @@ public class DataServlet extends HttpServlet {
     greetings.add("My favourite sport is swimming");
     greetings.add("Sleeping is important, please read 'Why We Sleep' by Matthew Walker");
   }
+  */
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String greeting = greetings.get((int) (Math.random() * greetings.size()));
+    /*String greeting = greetings.get((int) (Math.random() * greetings.size()));
     Gson gson = new Gson();
     String json = gson.toJson(greeting);
 
     response.setContentType("text/html;");
     response.getWriter().println(json);
+    */
+    String guess = request.getParameter("player-choice");
+    response.setContentType("application/json");
+    String json = new Gson().toJson(guess);
+    response.getWriter().println(json);
+
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the input from the form.
+    int playerChoice = getPlayerChoice(request);
+
+    if (playerChoice == 7) {
+      response.setContentType("text/html");
+      response.getWriter().println("You're correct!");
+
+      return;
+    }
+    
+    if (playerChoice == -1) {
+      response.setContentType("text/html");
+      response.getWriter().println("Please enter an integer between 1 and 10.");
+
+      return;
+    }
+
+    else{
+      response.setContentType("text/html");
+      response.getWriter().println("My favourite number from one to ten is 7.");
+ 
+      return;
+    }
+
+  }
+  /** Returns the choice entered by the player, or -1 if the choice was invalid. */
+  private int getPlayerChoice(HttpServletRequest request) {
+    // Get the input from the form.
+    String playerChoiceString = request.getParameter("player-choice");
+
+    // Convert the input to an int.
+    int playerChoice;
+    try {
+      playerChoice = Integer.parseInt(playerChoiceString);
+    } catch (NumberFormatException e) {
+      System.err.println("Could not convert to int: " + playerChoiceString);
+      return -1;
+    }
+
+    // Check that the input is between 1 and 10.
+    if (playerChoice < 1 || playerChoice > 10) {
+      System.err.println("Player choice is out of range: " + playerChoiceString);
+      return -1;
+    }
+
+    return playerChoice;
   }
 }
